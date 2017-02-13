e8841b66f69b8b57604445ca7fb6aec79a84d2b0
Jordan Whalen <jordan.whalen@soliddigital.com>
2017-02-12 20:44:33 -0500

    Working on home page

--- dev/js/scripts/window.js
+++ dev/js/scripts/window.js
@@ -1,4 +1,12 @@
+$(window).load( function() {
+
+});
+
 $(window).scroll(function() {
   var scrollTop = $(window).scrollTop();
   headerScroll(scrollTop > 70);
+});
+
+$(window).resize( function() {
+
 });
