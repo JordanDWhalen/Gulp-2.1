e9556c1a7c846c5eeab9a004d4d449b34a243964
Jordan Whalen <jordan.whalen@soliddigital.com>
2017-02-13 11:57:49 -0500

    Fixing hero video sizing/positioning

--- dev/js/scripts/window.js
+++ dev/js/scripts/window.js
@@ -7,6 +7,19 @@ $(window).scroll(function() {
   headerScroll(scrollTop > 70);
 });
 
-$(window).resize( function() {
+var $video  = $('.hero video'),
+    $window = $(window);
 
+$(window).resize( function() {
+  var height = $window.height();
+  $video.css('height', height);
+
+  var videoWidth = $video.width(),
+      windowWidth = $window.width(),
+      marginLeftAdjust =   (windowWidth - videoWidth) / 2;
+
+  $video.css({
+      'height': height,
+      'marginLeft' : marginLeftAdjust
+  });
 });
